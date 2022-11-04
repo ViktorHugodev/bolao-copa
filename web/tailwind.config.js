7fb285ae7a2ac5ff06b87be8d54e607eb90073a1
viktorhugodev <viktorhugo.dev@gmail.com>
2022-11-04 09:08:03 -0300

    add home page, axios, styles

---
+++ web/tailwind.config.js
@@ -0,0 +1,43 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx'
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        app: 'url(/bg.png)'
+      },
+      fontFamily: {
+        sans: 'Roboto, sans-serif',
+      },
+      colors: {
+        ignite: {
+          500: '#129E57'
+        },
+        gray: {
+          950: '#09090A',
+          900: '#121214',
+          800: '#202024',
+          600: '#323238',
+          300: '#8D8D99',
+          200: '#C4C4CC',
+          100: '#E1E1e6'
+        },
+        green: {
+          500: '#047C3F'
+        },
+        yellow: {
+          500: '#F7DD43',
+          600: '#BBA317',
+        },
+        red: {
+          500: '#DB4437',
+        },
+        white: '#ffff'
+      }
+    },
+
+  },
+  plugins: [],
+}
