da6e60fc74fa291a1e2a87637eb2f3f7b9993ed9
viktorhugodev <viktorhugo.dev@gmail.com>
2022-11-19 16:03:13 -0300

    add header

--- web/src/pages/login.tsx
+++ web/src/pages/login.tsx
@@ -32,22 +32,7 @@ export default function Login() {
                 </h2>
               </div>
               <div className='mt-16 grid space-y-4'>
-                <button
-                  onClick={signInGoogle}
-                  className='group h-12 px-6 border-2 border-gray-300 rounded-full transition duration-300 
- hover:border-blue-400 focus:bg-blue-50 active:bg-blue-100'
-                >
-                  <div className='relative flex items-center space-x-4 justify-center'>
-                    <img
-                      src='https://tailus.io/sources/blocks/social/preview/images/google.svg'
-                      className='absolute left-0 w-5'
-                      alt='google logo'
-                    />
-                    <span className='block w-max font-semibold tracking-wide text-gray-700 text-sm transition duration-300 group-hover:text-blue-600 sm:text-base'>
-                      Continue with Google
-                    </span>
-                  </div>
-                </button>
+             
               </div>
 
               <div className='mt-32 space-y-4 text-gray-600 text-center sm:-mb-8'>
